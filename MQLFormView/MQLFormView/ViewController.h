94b182cd8817d39a9bedf603019f7df015f3357d
孟庆岭 <m276675358@163.com>
2018-01-22 13:28:05 +0800

    创建工程

---
+++ MQLFormView/MQLFormView/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  MQLFormView
+//
+//  Created by GT-iOS on 2018/1/22.
+//  Copyright © 2018年 GT-iOS. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ MQLFormView/MQLFormView/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  MQLFormView
+//
+//  Created by GT-iOS on 2018/1/22.
+//  Copyright © 2018年 GT-iOS. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
